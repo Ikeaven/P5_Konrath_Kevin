b5d3f5d1171e1fe6d3b39d0e69dbb7543e0b9c69
Konrath <36306272+Ikeaven@users.noreply.github.com>
2021-08-05 11:13:55 +0200

    code source

---
+++ CODE_SOURCE/js/main.js
@@ -0,0 +1,32 @@
+import * as carousel from "./carousel.js";
+import * as utilities from "./utilities.js";
+
+
+let catégories = ['Action', 'Film-Noir', 'Sci-Fi']
+
+let global_url = "http://127.0.0.1:8000/api/v1/titles/"
+
+async function main(){
+    // display dynamique elements of the page
+    utilities.get_best_movie(global_url+"?sort_by=-imdb_score")
+    for (let categorie of catégories) {
+        await utilities.get_movie_by_categorie(global_url+"?genre="+categorie)
+            .then(array_films => {
+                carousel.create_carousel(array_films, categorie, false)
+            })
+}}
+
+
+window.addEventListener("load", ()=> {
+    // onLoad execut main()
+    main().then(()=>{
+        let section_best = document.querySelector('.best');
+        section_best.style.visibility = "visible";
+
+        let section_carousel = document.querySelector('.insert_carousel');
+        section_carousel.style.visibility = "visible";
+
+        let spinner = document.querySelector(".spinner");
+        spinner.style.display = "none";
+    })
+});
